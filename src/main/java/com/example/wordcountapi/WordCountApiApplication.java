77c6d24129fc20fc74f69d2fda0d4fd0536ecd8c
Leon <davidyu362000@gmail.com>
2022-10-14 17:07:32 +0200

    Initial commit

---
+++ src/main/java/com/example/wordcountapi/WordCountApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.wordcountapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordCountApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordCountApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wordcountapi/WordCountApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wordcountapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordCountApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
